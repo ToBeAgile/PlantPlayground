0b51c9d9483602f28b797a38837310c4e714c562
ToBeAgile <david@tobeagile.com>
2020-04-30 13:59:29 -0700

    Made sure ADS1115Reader and DataLogger are tracked

--- services/ADS1115Reader.py
+++ services/ADS1115Reader.py
@@ -1,40 +1,35 @@
 import Adafruit_ADS1x15
 import unittest
-import StreamInfo
 import time
         
 
 class ADS1115Reader:
     
     adc = Adafruit_ADS1x15.ADS1115()
-    stream_info = []
+    stream_info_dict = {0:6.144, 1:4.096, 2:2.048, 4:1.024, 8:0.512, 16:0.256}
+    status = 'close'
+    gain = 0
+    data_rate = 0
+    sleep = 1
+    channel = 0
     
     def __init__(self):
        #self.channel = channel
         dummy = 0
         
+        
     def open(self, channel, gain, data_rate, sleep):
-        si = StreamInfo.StreamInfo('open', gain, data_rate, sleep)
-        self.stream_info.append(si)
-         
-    def is_channel_open(self, channel):
-        #return channelCollection;
-        si = self.stream_info[channel]
-        return si.status
+        self.channel = channel
+        self.gain = gain
+        self.data_rate = data_rate
+        self.sleep = sleep
+        self.voltsPerDivision = ((2 * self.stream_info_dict[self.gain])/65535)*1000
     
-    def get_gain(self, channel):
-        return self.stream_info[channel].gain
-    
-    def get_data_rate(self, channel):
-        return self.stream_info[channel].data_rate
-    
-    def get_sleep(self, channel):
-        return self.stream_info[channel].sleep
-    
-    def read(self, channel):
-        dummy = self.adc.read_adc_difference(channel, self.get_gain(channel), self.get_data_rate(channel)) # Throw out first value
-        time.sleep(self.get_sleep(channel))
-        value = self.adc.read_adc_difference(channel, self.get_gain(channel), self.get_data_rate(channel))
+    def read(self):
+        dummy = self.adc.read_adc_difference(self.channel, self.gain, self.data_rate)
+        time.sleep(self.sleep)
+        value = self.adc.read_adc_difference(self.channel, self.gain, self.data_rate)
+        value *= self.voltsPerDivision
         return value
     
 
--- services/DataLogger.py
+++ services/DataLogger.py
@@ -1,6 +1,7 @@
 import datetime
 import os.path
 from os import path
+import csv
 
 class DataLogger:
     now = datetime.datetime.now()
@@ -10,9 +11,13 @@ class DataLogger:
     def __init__(self, partial_path="../data/"):
         self.partial_path = partial_path
         self.full_path = self.partial_path + self.filename
-        self.file = open(self.full_path, "w")
+        self.file = open(self.full_path, "a")
+        self.file.close()
         
     def write(self, data):
-        self.file.write(data)
+        #self.file.write(data)
+        with open(self.full_path, 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([data])
     
     
