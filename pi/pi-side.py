671317b2a6b1486cdb597e8e91be071856f437f2
amo25 <amo25@vt.edu>
2020-06-05 19:27:37 -0400

    Sensor graphing out to dash sucessfully across the network just over twice a second. Let the speed testing commence

--- pi/pi-side.py
+++ pi/pi-side.py
@@ -1,21 +1,24 @@
-#This is the side that will run on the pi. It should send data (lets have it send random data once a second)
-
 import socket
 import sys
 import time
 import pickle
-import random #for testing
-#sys.path.insert(1, '/home/pi/Documents/Code/PlantPlayground')
-from ADCStreamReader import ADCStreamReader
+import random
 import datetime
+import Adafruit_ADS1x15
 
-host = '192.168.0.18'
+# Create an ADS1115 ADC (16-bit) instance.
+adc = Adafruit_ADS1x15.ADS1115()
+GAIN = 16
+volts_per_division_table = {0:6.144, 1:4.096, 2:2.048, 4:1.024, 8:0.512, 16:0.256}
+mv_per_division = ((2 * volts_per_division_table[GAIN])/65535)*1000
+
+host = '192.168.1.14'
 port = 50000
 
 s = None
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host,port)) #remote server must already be running
+    s.connect((host,port))
 
 except socket.error as message:
     if s:
@@ -23,11 +26,10 @@ except socket.error as message:
     print ("Unable to open the socket: " + str(message))
     sys.exit(1)
     
-adcSR = ADCStreamReader()
-adc0 = adcSR.open(differential=0, gain=16, data_rate=8, sleep=0)
 
 sensor = "CH0"
-value = adcSR.read(adc0)
+#value = random.randrange(-20, 20)
+value = (adc.read_adc_difference(0, gain=GAIN, data_rate=128)) * mv_per_division
 now = datetime.datetime.now()
 sensor_time = now.strftime("%H:%M:%S:%f")
 
@@ -35,16 +37,13 @@ data_dict = {"sensor": sensor, "value": value, "time": sensor_time}
 serialized_data = pickle.dumps(data_dict)
 
 while True:
-    #todo set the above sensor, dict, serialized_data, etc here
-    value = random.randrange(-20, 20)
+    value = (adc.read_adc_difference(0, gain=GAIN, data_rate=128)) * mv_per_division
+    print(value)
     now = datetime.datetime.now()
     sensor_time = now.strftime("%H:%M:%S:%f")
-    #value = adcSR.read(adc0)
-    #print(value)
     data_dict = {"sensor": sensor, "value": value, "time": sensor_time}
     serialized_data = pickle.dumps(data_dict)
-    print("Sending...")
     s.send(serialized_data)
-    time.sleep(1)
+    time.sleep(0.4)
 
 s.close()
