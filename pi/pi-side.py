9aae28d9192b54153fd3f19aaee87a12a5e2d5bd
amo25 <amo25@vt.edu>
2020-06-15 14:19:46 -0400

    Threads up and running

--- pi/pi-side.py
+++ pi/pi-side.py
@@ -5,6 +5,7 @@ import pickle
 import random
 import datetime
 import Adafruit_ADS1x15
+import threading
 
 # Create an ADS1115 ADC (16-bit) instance.
 adc = Adafruit_ADS1x15.ADS1115()
@@ -12,7 +13,7 @@ GAIN = 16
 volts_per_division_table = {0:6.144, 1:4.096, 2:2.048, 4:1.024, 8:0.512, 16:0.256}
 mv_per_division = ((2 * volts_per_division_table[GAIN])/65535)*1000
 
-host = '192.168.1.14'
+host = '192.168.1.5'
 port = 50000
 
 s = None
@@ -28,22 +29,38 @@ except socket.error as message:
     
 
 sensor = "CH0"
-#value = random.randrange(-20, 20)
 value = (adc.read_adc_difference(0, gain=GAIN, data_rate=128)) * mv_per_division
 now = datetime.datetime.now()
 sensor_time = now.strftime("%H:%M:%S:%f")
 
-data_dict = {"sensor": sensor, "value": value, "time": sensor_time}
-serialized_data = pickle.dumps(data_dict)
 
-while True:
-    value = (adc.read_adc_difference(0, gain=GAIN, data_rate=128)) * mv_per_division
-    print(value)
-    now = datetime.datetime.now()
-    sensor_time = now.strftime("%H:%M:%S:%f")
-    data_dict = {"sensor": sensor, "value": value, "time": sensor_time}
-    serialized_data = pickle.dumps(data_dict)
-    s.send(serialized_data)
-    time.sleep(0.4)
+def read_sensor():
+    global sensor
+    global value
+    global sensor_time
 
-s.close()
+    while True:
+        value = (adc.read_adc_difference(0, gain=GAIN, data_rate=128)) * mv_per_division
+        print(value)
+        now = datetime.datetime.now()
+        sensor_time = now.strftime("%H:%M:%S:%f")
+        #time.sleep(.1) #todo depend on a user modified variable
+        read_event.wait(0.4)
+
+def write_network():
+    global sensor
+    global value
+    global sensor_time
+
+    while True:
+        write_event.wait(1)
+        print("Write: ", value)
+        data_dict = {"sensor": sensor, "value": value, "time": sensor_time}
+        serialized_data = pickle.dumps(data_dict)
+        s.send(serialized_data)
+
+read_event = threading.Event()
+write_event = threading.Event()
+#floats, ints, and dictionaries should all be thread safe in Python (floats and ints are immutable). Test this
+threading.Thread(target=read_sensor).start()
+threading.Thread(target=write_network).start()
