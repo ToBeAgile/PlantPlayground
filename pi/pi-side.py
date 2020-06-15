09205a845c7d6bf020e6b9a4132461238e5bbb40
amo25 <amo25@vt.edu>
2020-06-15 15:13:49 -0400

    Datalogging successfully implemented on the pi. Play around with speeds. Buffer currently set to 1=linebuffer. Sensor read currently 2.5 Hz, logging currently 2 Hz, network write+graphing 1Hz

--- pi/pi-side.py
+++ pi/pi-side.py
@@ -6,6 +6,9 @@ import random
 import datetime
 import Adafruit_ADS1x15
 import threading
+import csv
+#sys.path.insert(1, '.')
+#from services.DataLogger import DataLogger
 
 # Create an ADS1115 ADC (16-bit) instance.
 adc = Adafruit_ADS1x15.ADS1115()
@@ -39,13 +42,15 @@ def read_sensor():
     global value
     global sensor_time
 
+    #dl = DataLogger()
+
     while True:
         value = (adc.read_adc_difference(0, gain=GAIN, data_rate=128)) * mv_per_division
-        print(value)
         now = datetime.datetime.now()
         sensor_time = now.strftime("%H:%M:%S:%f")
-        #time.sleep(.1) #todo depend on a user modified variable
-        read_event.wait(0.4)
+        #log the data
+        #dl.write(str(time), str(value)) #this may be way too slow. Check implementation, might be reopening file every write. Even with a stream, might be too slow
+        read_event.wait(0.4) #todo depend on a user modified variable
 
 def write_network():
     global sensor
@@ -54,13 +59,34 @@ def write_network():
 
     while True:
         write_event.wait(1)
-        print("Write: ", value)
         data_dict = {"sensor": sensor, "value": value, "time": sensor_time}
         serialized_data = pickle.dumps(data_dict)
         s.send(serialized_data)
 
+def log_data():
+    global sensor
+    global value
+    global sensor_time
+
+    now = datetime.datetime.now()
+    project_name = "R0"
+    filename = project_name + "-" + now.strftime("%Y-%m-%d") + ".csv"
+    #full_path = "/data/" + filename
+    #with open(filename, 'a', newline='') as file:
+    file = open(filename, 'a', newline='', buffering=1)
+    writer = csv.writer(file)
+    writer.writerow(["Plant bioelectric data log. Project: Setup"])
+    writer.writerow(["Time", "Value"])
+    #for k in range(10):
+    while True:
+        log_event.wait(0.5)
+        writer.writerow([sensor_time, value])
+    file.close()
+
 read_event = threading.Event()
 write_event = threading.Event()
+log_event = threading.Event()
 #floats, ints, and dictionaries should all be thread safe in Python (floats and ints are immutable). Test this
 threading.Thread(target=read_sensor).start()
 threading.Thread(target=write_network).start()
+threading.Thread(target=log_data).start()
