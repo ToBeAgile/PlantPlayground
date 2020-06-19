5c59d7557c5a314ccb15d116e84bd07e55c51df4
amo25 <amo25@vt.edu>
2020-06-19 16:04:15 -0400

    Stable dual channel, with spurious bad data. Improve time slicing. Adding dash-sockets-mac

--- application/dash-sockets-mac.py
+++ application/dash-sockets-mac.py
@@ -17,7 +17,7 @@ import time
 host = ''
 port = 50000
 backlog = 5
-size = 1024
+size = 5096
 
 #dl = DataLogger()
 
@@ -39,10 +39,15 @@ except socket.error as message:
     sys.exit(1)
 
 
-X = deque(maxlen=20)
-X.append(1)
-Y = deque(maxlen=20)
-Y.append(1)
+a_time_q = deque(maxlen=300)
+a_time_q.append(1)
+a_value_q = deque(maxlen=300)
+a_value_q.append(1)
+
+b_time_q = deque(maxlen=300)
+b_time_q.append(1)
+b_value_q = deque(maxlen=300)
+b_value_q.append(1)
 
 
 app = dash.Dash(__name__)
@@ -61,29 +66,35 @@ app.layout = html.Div(
               [Input('graph-update', 'n_intervals')])
 def update_graph(n):
     data = plotly.graph_objs.Scatter(
-            x=list(X),
-            y=list(Y),
+            x=list(a_time_q),
+            y=list(a_value_q),
             name='Scatter',
             mode= 'lines+markers'
             )
 
-    return {'data': [data],'layout' : go.Layout(xaxis=dict(range=[min(X),max(X)]),
-                                                yaxis=dict(range=[min(Y),max(Y)]),)}
+    return {'data': [data],'layout' : go.Layout(xaxis=dict(range=[(max(a_time_q)-50),max(a_time_q)]),
+                                                yaxis=dict(range=[(min(a_value_q)-5),max(a_value_q)+5]),)}
 
 
 def update_data():
     while True:
         serialized_data = client.recv(size)
-        value = pickle.loads(serialized_data)
-        #data_dict = pickle.loads(serialized_data)
-        #sensor = data_dict["sensor"]
+        #value = pickle.loads(serialized_data)
+        try: #deal with occasional broken network data
+            data_dict = pickle.loads(serialized_data)
+            sensor = data_dict["sensor"]
+        except:
+            data_dict = {"sensor": "junk", "value": 0, "time": 0}
+            print("Bad Network Data")
+        if sensor == "a_sensor":
+            a_time = data_dict["time"]
+            a_value = data_dict["value"]
+            a_time_q.append(a_time_q[-1]+1)
+            a_value_q.append(a_value)
         #value = data_dict["value"]
         #time = data_dict["time"]
         #print("Time: ", time, " Value: ", value)
         #dl.write(str(time), str(value))
-        X.append(X[-1]+1)
-        Y.append(value)
-
 
 if __name__ == '__main__':
     threading.Thread(target=update_data).start()
