0aa10e6b0c1c587cbf93b6ee1c48d63b5427dd76
amo25 <amo25@vt.edu>
2020-06-21 22:33:43 -0400

    Added second graph. Data still a bit messy with two channels being read on the same ADC. Pi side

--- pi/pi-side.py
+++ pi/pi-side.py
@@ -11,9 +11,9 @@ import csv
 #from services.DataLogger import DataLogger
 
 #Set the rates. Implement these into a GUI
-sensor_read_frequency = 120.0 #Hz
-network_write_frequency = 20 #Hz    How many data points will be graphed each second
-data_log_frequency = 2.0 #Hz  How many data points are logged each second locally, on the pi
+sensor_read_frequency = 20.0 #Hz
+network_write_frequency = 10.0 #Hz    How many data points will be graphed each second
+data_log_frequency = 0.01 #Hz  How many data points are logged each second locally, on the pi
 
 #Calculated from above
 sensor_read_time = float(1/sensor_read_frequency)
@@ -24,11 +24,12 @@ data_log_time = float(1/data_log_frequency)
 adc = Adafruit_ADS1x15.ADS1115()
 a_gain = 16
 b_gain = 2/3
-a_data_rate = 475
-b_data_rate = 475
+a_data_rate = 128
+b_data_rate = 128
 volts_per_division_table = {2/3:6.144, 1:4.096, 2:2.048, 4:1.024, 8:0.512, 16:0.256}
 mv_per_division = ((2 * volts_per_division_table[a_gain])/65535)*1000
 b_mv_per_division = ((2 * volts_per_division_table[b_gain])/65535)*1000
+sensor_state = 0
 
 #Initialize the shared variables across threads
 a_value = (adc.read_adc_difference(0, gain=a_gain, data_rate=a_data_rate)) * mv_per_division
@@ -58,16 +59,28 @@ def read_sensor():
     global a_time
     global b_value
     global b_time
+    global sensor_state
 
     while True:
-        #todo wrap this up in a class
-        a_value = (adc.read_adc_difference(0, gain=a_gain, data_rate=128)) * mv_per_division
-        a_time = datetime.datetime.now().strftime("%H:%M:%S:%f")
-        #read_event.wait(0.01) #this is probably what's causing the problems
-        b_value = (adc.read_adc_difference(3, gain=b_gain, data_rate=128)) * b_mv_per_division
-        b_time = datetime.datetime.now().strftime("%H:%M:%S:%f")
-        #print("A time, value: ", a_time, a_value)
-        #print("B time, value: ", b_time, b_value)
+        #a_value = (adc.read_adc_difference(0, gain=a_gain, data_rate=a_data_rate)) * mv_per_division
+        #a_time = datetime.datetime.now().strftime("%H:%M:%S:%f")
+        
+        
+        if sensor_state == 0:
+            a_value = (adc.read_adc_difference(0, gain=a_gain, data_rate=a_data_rate)) * mv_per_division
+            a_time = datetime.datetime.now().strftime("%H:%M:%S:%f")
+            sensor_state = sensor_state + 1
+            #print("A time, value: ", a_time, a_value)
+        elif sensor_state == 1:
+            b_value = (adc.read_adc_difference(3, gain=b_gain, data_rate=b_data_rate)) * b_mv_per_division
+            b_time = datetime.datetime.now().strftime("%H:%M:%S:%f")
+            sensor_state = 0
+            #print("B time, value: ", b_time, b_value)
+        else:
+            print("Sensor state machine error!")
+            sensor_state = 0
+        
+        
         read_event.wait(sensor_read_time) #todo depend on a user modified variable
 
 def write_network():
