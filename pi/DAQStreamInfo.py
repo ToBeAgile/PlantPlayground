52e2a6e2faa65c0f1e6b44b47a762b81d1f04b12
ToBeAgile <dave@tobeagile.com>
2022-05-23 09:36:17 -0600

    f moved DAQStreamInfo to its own file for reading .ini file.

---
+++ pi/DAQStreamInfo.py
@@ -0,0 +1,81 @@
+# DAQStreamInfo.py 1.0 - May 23, 2022 (c)reated by David Scott Bernstein
+# DAQStreamInfo.py - config file handling called by DAQStreams.py
+import configparser
+import ast
+
+ini_file_name = 'DAQStreams.ini'
+
+class DAQStreamInfo:
+
+    def __init__(self):
+        self.daq_to_use = None
+        self.sensor_type = None
+        self.sensor_read_frequency = None
+        self.number_of_channels = None
+        self.data_log_frequency = None
+        self.sensor_read_frequency = None
+        self.network_write_frequency = None
+        self.to_log = None
+        ###
+        self.sleep_between_reads = None
+        sleep_between_channels = None
+        number_of_channels = None
+        low_chan = None
+        high_chan = None
+        channels = []
+        sensor_type = None
+        ###
+        self.analog_input_range = None
+        self.reader_type = None
+        self.options = None
+        self.input_mode = None
+        self.input_range = None
+        self.daq = None
+        self.device = None
+        self.num_channels = None
+        self.hat_error = None
+        self.sample_interval = 0.0
+        ###
+        self.gain = None
+        self.data_rate = None
+        self.ads1115_sensor_type = None
+        self.ads1256_sensor_type = None
+        self.scan_mode = 0
+
+    def getConfig(self, ini_file_name):
+        config = configparser.ConfigParser()
+        config.read(ini_file_name)
+        # General settings
+        self.daq_to_use = config['Default']['daq_to_use']
+        self.ads1115_sensor_type = config['Default']['ads1115_sensor_type']
+        self.number_of_channels = config['Default']['number_of_channels']
+        self.data_log_frequency = int(config['Default']['data_log_frequency'])
+        self.sensor_read_frequency = config['Default']['sensor_read_frequency']
+        self.network_write_frequency = config['Default']['network_write_frequency']
+        self.to_log = config['Default']['to_log']
+        ###
+        self.sleep_between_reads = int(config['Default']['sleep_between_reads'])
+        self.sleep_between_channels = float(config['Default']['sleep_between_channels'])
+        self.number_of_channels = int(config['Default']['number_of_channels'])
+        self.low_chan = int(config['Default']['low_chan'])
+        self.high_chan = int(config['Default']['high_chan'])
+        self.channels = ast.literal_eval(config.get('Default', 'channels'))
+        self.sensor_type = config['Default']['sensor_type']
+        #MCC128 specific
+        self.analog_input_range = config['Default']['analog_input_range']
+        self.reader_type = config['Default']['reader_type']
+        self.options = config['Default']['options']
+        self.input_mode = config['Default']['mcc_input_mode']
+        self.input_range = config['Default']['input_range']
+        self.daq = config['Default']['DAQ']
+        self.device = config['Default']['Device']
+        self.num_channels = config['Default']['NumChannels']
+        self.hat_error = config['Default']['mcc_hat_error']
+        # ADS1115 settings
+        self.gain = config['Default']['gain']
+        self.data_rate = config['Default']['data_rate']
+        self.ads1115_sensor_type = config['Default']['ads1115_sensor_type']
+        # ADS1256 settings
+        self.ads1256_sensor_type = config['Default']['ads1256_sensor_type']
+        self.ScanMode = config['Default']['ScanMode']
+        return self
